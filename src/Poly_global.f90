510cab9955b85a7c4d954ad66889ad251ad481ca
Luca Brambilla <luca13.brambilla@mail.polimi.it>
2024-12-10 11:18:07 +0100

    move global_parameters module into Poly_global.f90

--- src/Poly_global.f90
+++ src/Poly_global.f90
@@ -371,7 +371,53 @@ subroutine calc_time(time_h, time_m, time_s, time_in_seconds)
 end subroutine calc_time
 
 
+!> Global constants
+module global_parameters
 
+    implicit none
 
+    type :: ScatteredArray
+        real(kind=8), dimension(:), allocatable :: data
+    end type ScatteredArray
 
+    integer(kind=4), parameter :: DIM = 3               !< dimension of the problem
+    real(kind=8), parameter :: SQRT2 = sqrt(2.0d0)      !< parameter for @f$ \sqrt{2} @f$
+    real(kind=8), parameter :: PI = 4.d0*datan(1.0d0)   !< parameter for @f$ \pi @f$
+    real(kind=8), parameter :: TOL = 1.0d-40            !< tolerance for small numbers
 
+end module global_parameters
+
+subroutine STOP_LYMPH3D
+
+    use Poly_setup_MPI
+    use Poly_global
+
+    implicit none
+
+    call MPI_BARRIER(MPI_COMM_WORLD, mpi_ierr)
+
+    finish = MPI_WTIME()
+    call calc_time(time_hour, time_min, time_sec, int(finish-start))
+
+    if (mpi_id == 0) then
+        write(*,'(A)')'--------------- SIMULATION WAS STOPPED ----------------'
+        write(*,'(A)')
+        if (IS_MatrixFree .eqv. .true.) then
+            write(*,'(A)')'Matrix free'
+        else
+            write(*,'(A)')'PETSc full'
+        endif
+        write(*,'(A)')
+        write(*,'(A)')'-------------------------------------------------------'
+        write(*,'(A,I2,A,I2,A,I2,A)') &
+                'Set-up time = ', time_hour,' h ' , time_min,' m ' , time_sec,' s'
+        write(*,'(A)')'-------------------------------------------------------'
+        write(*,'(A)')
+    endif
+
+    call MPI_BARRIER(MPI_COMM_WORLD, mpi_ierr)
+    call PetscFinalize(mpi_ierr)
+    call MPI_FINALIZE(mpi_ierr)
+    stop
+
+end subroutine STOP_LYMPH3D
