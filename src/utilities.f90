f0b69b672a4f39f933e80b821260236580d0c81d
Luca Brambilla <luca13.brambilla@mail.polimi.it>
2024-12-12 11:59:15 +0100

    add utility to delete files if program was ran with different number of processes - and utility to print fortran solution to file

--- src/utilities.f90
+++ src/utilities.f90
@@ -1,6 +1,7 @@
 module utilities
 
 #include<petsc/finclude/petscmat.h>
+use Poly_setup_MPI
 
 implicit none
 contains
@@ -8,7 +9,6 @@ contains
 !> save full PETSc matrix to a file, row by row
 subroutine save_matrix(matrix, nrows, ncols, filename)
     use petscmat
-    use Poly_setup_MPI
 
     implicit none
 
@@ -39,7 +39,71 @@ subroutine save_matrix(matrix, nrows, ncols, filename)
 
 end subroutine
 
-subroutine save_vector
+!> save solution vector, element by element
+subroutine save_vector(vector, nrows, filename)
+    implicit none
+
+    integer(kind=4), intent(in) :: nrows
+    real(kind=8), dimension(:,:), intent(in) :: vector
+    character(len=*), intent(in) :: filename
+
+    integer(kind=4) :: i, unit_print
+
+    open(newunit=unit_print, action='WRITE', file=filename, &
+    form='FORMATTED', status='replace')
+    do i=1,nrows
+        write(unit_print, *) vector(i,:)
+    enddo
+    close(unit=unit_print)
+
 end subroutine
+
+!> Check files in the folder FILES_MPI and delete if the program was previously
+!> ran with a different number of processes
+subroutine check_mpi_files
+
+    implicit none
+
+    logical :: file_exists
+    character(len=100) :: filename
+
+    ! checking mesh files
+    ! A required file is missing; delete all files
+    write(filename, '("FILES_MPI/mesh_", i6.6, ".mpi")') mpi_np-1
+    inquire(file=filename, exist=file_exists)
+    if (.not. file_exists) then
+        print *, "Program previously ran with fewer processes than", mpi_np
+        call delete_all_files()
+        return
+    end if
+
+    ! Check for any extra files
+    write(filename, '("FILES_MPI/mesh_", i6.6, ".mpi")') mpi_np
+    inquire(file=filename, exist=file_exists)
+    if (file_exists) then
+        print *, "Program previously ran with more processes than", mpi_np
+        call delete_all_files()
+    end if
+
+end subroutine check_mpi_files
+
+!> Delete all files in the FILES_MPI directory
+subroutine delete_all_files()
+
+    implicit none
+
+    character(len=200) :: command
+
+    ! System command to delete all files
+    command = "rm -f FILES_MPI/*"
+    call execute_command_line(command, wait=.true., exitstat=ierr)
+
+    if (ierr /= 0) then
+        print *, "Error deleting files. Exit code:", ierr
+    else
+        print *, "All files deleted in FILES_MPI directory."
+    end if
+end subroutine delete_all_files
+
 
 end module utilities
