a0ec6efb8777978d6e210566c2b80079f976329a
Luca Brambilla <luca13.brambilla@mail.polimi.it>
2025-02-08 12:57:13 +0100

    fix installation

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,14 +4,15 @@ LYMPH_DIR="$(pwd)"
 
 # METIS
 cd metis-5.1.0
-make config prefix=.
-make install
+make config #prefix=.
+make
+sudo make install
 
 # FMETIS
 cd ../fmetis
 mkdir -p build
-#export FMETIS_DIR
 cd build
-cmake .. -DMETIS_LIB="$LYMPH_DIR/metis-5.1.0/build/Linux-x86_64/lib/libmetis.a" -DREAL=64 -DCMAKE_INSTALL_PREFIX=./installed
-make install
-ctest
+rm -rf *
+cmake .. -DMETIS_LIB="/usr/local/lib/libmetis.a" -DREAL=64 #-DCMAKE_INSTALL_PREFIX=./installed
+sudo make install
+ctest --rerun-failed --output-on-failure
