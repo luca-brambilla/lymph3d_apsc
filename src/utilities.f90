d6640dff35626cfffaa47aa41427127c7f888b1e
Luca Brambilla <luca13.brambilla@mail.polimi.it>
2025-02-11 19:43:59 +0100

    remove mesh visualization files

--- src/utilities.f90
+++ src/utilities.f90
@@ -138,7 +138,7 @@ subroutine DELETE_ALL_FILES
     integer(kind=4) :: ierr
 
     ! System command to delete all files
-    command = "rm -f FILES_MPI/* MONITORS/*"
+    command = "rm -f FILES_MPI/* MONITORS/* mesh_visualization/*"
     call EXECUTE_COMMAND_LINE(command, wait=.true., exitstat=ierr)
 
     if (ierr /= 0) then
